86a88e363f0e3b32406174809311f145cd94fb9e
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-12-25 20:44:47 -0800

    add passport.serializeUser/deserializeUser

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,17 @@ const keys = require('../config/keys');
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);  //user.id is mongodb id, not gooleId
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
+
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
@@ -12,17 +23,17 @@ passport.use(new GoogleStrategy({
   }, (accessToken, refreshToken, profile, done) => {
 
     // check if user exists
-    User.findOne({ googleId: profile.id })
-      .then( (existingUser) => {
-        if (existingUser) {
-          // user already exist
-          
-        } else {
-          // no user exist
-          new User({ googleId: profile.id }).save();  //create new instance of User mongoose and save
-        }
-      })
-
+    User.findOne({ googleId: profile.id }).then( (existingUser) => {
+      if (existingUser) {
+        // user already exist
+        done(null, existingUser);   // no error -> pass null as a first argument
+      } else {
+        // no user exist
+        new User({ googleId: profile.id })  //create new instance of User mongoose
+          .save() //save to database
+          .then(user => done(null, user));
+      }
+    });
 
   })
 );
