f629280d4c612daefa0df4f086480fcaa552fedc
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-12-25 20:51:33 -0800

    add cookieSession

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 
 // require User first, then passport. because passport uses User
@@ -8,6 +10,18 @@ require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 const app = express();
+
+//tell express to use cookieSession
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,   // cookie expires in 30days
+    keys: [keys.cookieKey]
+  })
+);
+
+// tell express to use passport cookie session
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
