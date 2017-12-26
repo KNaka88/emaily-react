36b08d265258c0e6525b9de90161435b151ef859
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-12-25 21:02:30 -0800

    add logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,4 +10,15 @@ module.exports = app => {
 
   // when google send back to callbackURL, pass to passport
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user); // this returns user info
+  });
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();  // tell passport logout and kills cookies
+    res.send(req.user); // this should return empty
+  });
+
 };
