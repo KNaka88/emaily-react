2113cb2e892129bfa5adf3abd090ebadbfd9715e
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-12-28 17:53:25 -0800

    fix bug

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   // production mode
-  module.exports = require('/prod');
+  module.exports = require('./prod');
 } else {
   // development mode
   module.exports = require('./dev');
