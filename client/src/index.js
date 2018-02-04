3a61f9596e4cf5ed90ae1a710b542736b1098c7c
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-02-04 12:29:35 -0800

    set up react

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDom.render();
+import App from './components/App';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root'));
