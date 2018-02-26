8a62236d9f811cf4823e00286887c09ab4a8c1c8
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-02-25 21:17:46 -0800

    reafactor action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export const fetchUser = () => {
-    return function(dispatch) {
-      axios
-        .get('/api/current_user')
-        .then(res => dispatch({ type: FETCH_USER, payload: res }));
-    }
+export const fetchUser = () => async (dispatch) => {
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res });
 };
