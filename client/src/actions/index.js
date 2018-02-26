ed805c195c2bf6ea861c63b3dd5d11a1e036c292
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-02-25 21:47:41 -0800

    add login/logout functionallity on header

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,5 +5,5 @@ import * as actions from '../actions';
 
 export const fetchUser = () => async (dispatch) => {
   const res = await axios.get('/api/current_user');
-  dispatch({ type: FETCH_USER, payload: res });
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
